be1e195a72e811810bed208667fad09763a140d9
Edson Guilherme de Almeida <39169184+UNRBold@users.noreply.github.com>
2018-12-04 18:01:53 -0200

    Web-Site

--- API2/API2/wwwroot/Site/assets/js/main.js
+++ API2/API2/wwwroot/Site/assets/js/main.js
@@ -1,29 +1,51 @@
-$(document).ready(function(){
-    $("a").on('click', function(event) {
+//$(document).ready(function(){
+   // $("a").on('click', function(event) {
   
      
-      if (this.hash !== "") {
+    //  if (this.hash !== "") {
 
-        event.preventDefault();
+      //  event.preventDefault();
   
        
-        var hash = this.hash;
+      //  var hash = this.hash;
 
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-     
+      //  $('html, body').animate({
+     //    scrollTop: $(hash).offset().top
+      //  }, 800, function(){
+     //
         
-          window.location.hash = hash;
-        });
-      } 
-    });
-  });
+        //  window.location.hash = hash;
+       // });
+    //  } 
+  // });
+  // });
 
-  function topo()
+function scrollbar(b)
+{
+  a = b;
+  if(a == "blog")
+  {
+    $('html, body').animate({scrollTop:1500}, 'slow');
+  }
+  else if(a == "home")
   {
     $('html, body').animate({scrollTop:0}, 'slow'); 
   }
+  else if(a== "about")
+  {
+    $('html, body').animate({scrollTop:2150}, 'slow');
+  }
+  else if(a =="partners")
+  {
+    $('html, body').animate({scrollTop:3000}, 'slow');
+  }
+  else if(a=="services")
+  {
+    $('html, body').animate({scrollTop:600}, 'slow');
+  }
+}
+
+  
 function openclose(el)
  {
   var display = document.getElementById(el).style.display;
